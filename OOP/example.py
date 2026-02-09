7881ca4be21dc7988bfed32ec11dea1d372ce946
ahmedrehman24 <ahmedrehman242002@gmail.com>
2026-02-09 17:36:42 +0500

    updates

--- OOP/example.py
+++
@@ -1,1 +0,0 @@
-#adding oop practice files
--- python/two_d_collections.py
+++ python/two_d_collections.py
@@ -1,4 +1,8 @@
-matrix = [[i * j for j in range(1, 6)] for i in range(1, 6)]
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
 for row in matrix:
     print(row)
